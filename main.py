ee4f0897f095c54d8a20c2d7099ccfbd0e23aa37
xeonel <xeone@asia.com>
2018-12-15 22:27:41 +0000

    plot length of mail

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def mainfunc():
         print('Please pass the correct argument. (initialize, clean, explore, train...)')
     elif sys.argv[1] == 'initialize':
         getdataset()
-    elif sys.argv[1] == 'clean':
-        clean()
+    elif sys.argv[1] == 'extract':
+        extract()
     elif sys.argv[1] == 'explore':
         explore_data()
     else:
@@ -104,7 +104,7 @@ def getdataset():
     print("Initialized and parsed datasets to training.df and test.df")
     return
 
-def clean():
+def extract():
     print('Training using training.df')
     with open('dataset/training.df', 'rb') as training_file:
         trainingdf = pickle.load(training_file)
@@ -123,16 +123,30 @@ def clean():
     spamcv, dtmspam = dtm(smessages)
     hamcv, dtmham = dtm(hmessages)
 
-    # print(spamcv.get_feature_names())
     spamdist = np.sum(dtmspam, axis=0)
     hamdist = np.sum(dtmham, axis=0)
 
 
-
+    #Dumping the word frequencies for visualization and training
     with open('dataset/spamdist.ps', 'wb') as spamdistfile:
         pickle.dump(spamdist, spamdistfile)
     with open('dataset/hamdist.ps', 'wb') as hamdistfile:
         pickle.dump(hamdist, hamdistfile)
+
+
+    #Getting the length of emails
+    smsgcounts = pd.Series(smsgsdf["content"])
+    smsgcounts = list(map(lambda x: len(x) , smsgcounts))
+
+    hmsgcounts = pd.Series(hmsgsdf["content"])
+    hmsgcounts = list(map(lambda x: len(x) , hmsgcounts))
+
+    #Dumping word length for visualization
+    with open('dataset/spamlengths.list', 'wb') as spamlengthsfile:
+        pickle.dump(smsgcounts, spamlengthsfile)
+    with open('dataset/hamlengths.list', 'wb') as hamlengthsfile:
+        pickle.dump(hmsgcounts, hamlengthsfile)
+
     return
 
 
@@ -143,17 +157,32 @@ def explore_data():
     with open('dataset/hamdist.ps', 'rb') as hamdistfile:
         hamdist = pickle.load(hamdistfile)
     
+    #Plot spam frequency
     bar_chart_spam = pygal.HorizontalBar()
     bar_chart_spam.x_labels = spamdist.sort_values(ascending=False)[:20].keys()
     bar_chart_spam.add('spam', spamdist.sort_values(ascending=False)[:20].values)
     bar_chart_spam.render_in_browser()
 
+    #Plot ham frequency
     bar_chart_ham = pygal.HorizontalBar()
     bar_chart_ham.x_labels = hamdist.sort_values(ascending=False)[:20].keys()
     bar_chart_ham.add('ham', hamdist.sort_values(ascending=False)[:20].values)
     bar_chart_ham.render_in_browser()
+
+
+    with open('dataset/spamlengths.list', 'rb') as spamlengthsfile:
+        smsgcounts = pickle.load(spamlengthsfile)
+    with open('dataset/hamlengths.list', 'rb') as hamlengthsfile:
+        hmsgcounts = pickle.load(hamlengthsfile)
+
+    #plot length of emails
+    pyramid_chart = pygal.Pyramid(human_readable=True, legend_at_bottom=True)
+    pyramid_chart.title = 'Number of meaningful words'
+    pyramid_chart.add('Spam', smsgcounts)
+    pyramid_chart.add('Ham', hmsgcounts)
+    pyramid_chart.render_in_browser()
     return
-    
+
 
 
 mainfunc()
